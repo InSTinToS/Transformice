ecf5091f365d7ef1d62589b192dfca4653ff6fe8
Miguel Andrade Barreto <miguelandradebarreto2@gmail.com>
2019-10-25 12:37:06 -0300

    Added bitmaps

--- Origem.cpp
+++ Origem.cpp
@@ -3,6 +3,7 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_image.h>
 
 int main () 
 {
@@ -10,9 +11,10 @@ int main ()
 	al_init_primitives_addon();
 	al_init_font_addon();
 	al_init_ttf_addon();
+	al_init_image_addon();
 
-	int width = 1080;
-	int height = 720;
+	int width = 900;
+	int height = 900;
 	int tamFont = 32;
 	float x1, x2, y1, y2,porcent,posTexto,border;
 	
@@ -24,19 +26,29 @@ int main ()
 	x2 = width	-(width * porcent);
 	y2 = height	-(height * porcent);
 
+
 	ALLEGRO_DISPLAY* display = NULL;
 	ALLEGRO_FONT* font = NULL;
+	ALLEGRO_BITMAP* mouse = NULL;
+	ALLEGRO_BITMAP* cheese = NULL;
+	ALLEGRO_BITMAP* lair = NULL;
 	ALLEGRO_COLOR lightBrown = al_map_rgb(210, 180, 140);
 	ALLEGRO_COLOR darkBrown = al_map_rgb(139, 69, 19);
 	ALLEGRO_COLOR skyBlue = al_map_rgb(176, 224, 230);
-
-	font = al_load_font("edit/soopafre.ttf", tamFont,NULL);
+	
 	display = al_create_display(width, height);
+	font = al_load_font("edit/font/soopafre.ttf", tamFont,NULL);
+	mouse = al_load_bitmap("edit/img/rato.png");
+	cheese = al_load_bitmap("edit/img/queijo.png");
+	lair = al_load_bitmap("edit/img/toca.png");
 
 	al_draw_filled_rectangle(0,0, width, height, skyBlue);
 	al_draw_filled_rectangle(x1, y1, x2, y2, lightBrown);
 	al_draw_rectangle(x1, y1, x2, y2, darkBrown,border);
 
+	al_draw_bitmap(mouse, 400, 360, NULL);
+	al_draw_bitmap(cheese, 300, 360, NULL);
+	al_draw_bitmap(lair, 200, 360, NULL);
 	al_draw_text(font, darkBrown, width / 2, posTexto, ALLEGRO_ALIGN_CENTRE, "T R A N S F O R M I C E");
 
 	al_flip_display();
