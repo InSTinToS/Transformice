3aeef272c46fc361580a8aab94debdd8f96f1abe
Miguel Andrade Barreto <miguelandradebarreto2@gmail.com>
2019-10-25 13:52:52 -0300

    Starting random challenge

--- Origem.cpp
+++ Origem.cpp
@@ -3,16 +3,43 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_image.h>
 
-
-#include <cstdlib>
 #include <iostream>   
 #include <ctime>
 
 
+int getRandom ()
+{
+	static int Init = 0;
+	int min = 90;
+	int max = 810 - 50;
+	if (Init == 0)
+	{
+		srand(time(NULL));
+		Init = 1;
+	}
 
-int main () 
+	return (rand() % (max - min + 1) + min);
+}
+
+void formWalls(ALLEGRO_COLOR cor)
+{
+	int randomX, randomY, randomX2, randomY2;
+	for (int i = 0; i < 10; i++)
+	{
+		randomX = getRandom();
+		randomY = randomX;
+		randomX2 = randomX + 50;
+		randomY2 = randomY + 50;
+		int aux = getRandom();
+		if (randomX <= aux + 50 && randomX >= aux)
+			i--;
+		else
+			al_draw_filled_rectangle(randomX, randomY, randomX2, randomY2, cor);
+	}
+}
+
+int main()
 {
 	al_init();
 	al_init_primitives_addon();
@@ -20,19 +47,26 @@ int main ()
 	al_init_ttf_addon();
 	al_init_image_addon();
 
+
 	int width = 900;
 	int height = 900;
 	int tamFont = 32;
-	int randomX, randomY;
-	float x1, x2, y1, y2,porcent,posTexto,border;
-	
-	porcent = 0.1;
-	posTexto = (height * (porcent / 2)) - (tamFont / 2) - 5;
+
+	float x1, x2, y1, y2, posTexto, border;
+
+
+
+	posTexto = (height * 0.05) - (tamFont / 2) - 5;
 	border = ((width * 0.05) / 5);
-	x1 = width	* porcent;
-	y1 = height * porcent;
-	x2 = width	-(width * porcent);
-	y2 = height	-(height * porcent);
+	x1 = 90;
+	y1 = 90;
+	x2 = width - 90;
+	y2 = height - 90;
+
+
+
+
+
 
 
 	ALLEGRO_DISPLAY* display = NULL;
@@ -43,28 +77,27 @@ int main ()
 	ALLEGRO_COLOR lightBrown = al_map_rgb(210, 180, 140);
 	ALLEGRO_COLOR darkBrown = al_map_rgb(139, 69, 19);
 	ALLEGRO_COLOR skyBlue = al_map_rgb(176, 224, 230);
-	
+
 	display = al_create_display(width, height);
-	font = al_load_font("edit/font/soopafre.ttf", tamFont,NULL);
+	font = al_load_font("edit/font/soopafre.ttf", tamFont, NULL);
 	mouse = al_load_bitmap("edit/img/rato.png");
 	cheese = al_load_bitmap("edit/img/queijo.png");
 	lair = al_load_bitmap("edit/img/toca.png");
 
-	al_draw_filled_rectangle(0,0, width, height, skyBlue);
+	al_draw_filled_rectangle(0, 0, width, height, skyBlue);
 	al_draw_filled_rectangle(x1, y1, x2, y2, lightBrown);
-	al_draw_rectangle(x1, y1, x2, y2, darkBrown,border);
+	al_draw_rectangle(x1, y1, x2, y2, darkBrown, border);
 
 	al_draw_bitmap(mouse, 400, 360, NULL);
 	al_draw_bitmap(cheese, 300, 360, NULL);
 	al_draw_bitmap(lair, 200, 360, NULL);
 	al_draw_text(font, darkBrown, width / 2, posTexto, ALLEGRO_ALIGN_CENTRE, "T R A N S F O R M I C E");
-	
-	al_draw_filled_rectangle(randomX, randomY, randomX + 10, randomY + 10, darkBrown);
+
+	formWalls(darkBrown);
 
 	al_flip_display();
-	al_rest(20);
+	al_rest(3);
 	al_destroy_display(display);
 	
-
 	return 0;
 }
